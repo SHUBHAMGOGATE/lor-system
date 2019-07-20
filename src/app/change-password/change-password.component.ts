bc1a324a999b7a6e09c54f19e6a9d04ba4c95deb
Manas Sinkar <manas.sinkar@gmail.com>
2019-07-20 21:54:20 +0530

    Change Password issue solved

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -48,7 +48,7 @@ export class ChangePasswordComponent implements OnInit,OnDestroy {
 
 
   ngOnInit() {
-    if(this.authService.currentUserValue.email){
+    if(this.authService.currentUserValue){
       this.email_form.value.email=this.authService.currentUserValue.email;
       this.sendEmail();
     }
